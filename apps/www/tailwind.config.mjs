4fc8e06fca6df79bd8663d676741ae688342c1cd
kmrk <kmrkgo@gmail.com>
2024-10-06 14:23:01 +0800

    from ui lib

--- apps/www/tailwind.config.mjs
+++ apps/www/tailwind.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+//import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+import baseConfig from "@acme/tailwind-config/web";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    ...baseConfig.content,
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    "../../packages/ui/**/*.{ts,tsx}"],
 	theme: {
 		extend: {},
 	},
